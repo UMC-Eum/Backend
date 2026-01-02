d78da0aa41d4a65a9f400e2305854f90719ec1db
ehgus3193 <ehgus3193@naver.com>
2026-01-02 17:19:21 +0900

    feat : 파일 모듈 구조 생성(profile -> onboarding , 마스터 데이터 모듈 제거 , compliance -> agreement) crud 구현 제거

---
+++ src/modules/social/controllers/heart/heart.controller.ts
@@ -0,0 +1,4 @@
+import { Controller } from '@nestjs/common';
+
+@Controller('heart')
+export class HeartController {}
---
+++ src/modules/social/social.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { HeartController } from './controllers/heart/heart.controller';
+import { BlockController } from './controllers/block/block.controller';
+import { ReportController } from './controllers/report/report.controller';
+import { BlockService } from './services/block/block.service';
+import { HeartService } from './services/heart/heart.service';
+import { ReportService } from './services/report/report.service';
+
+@Module({
+  controllers: [HeartController, BlockController, ReportController],
+  providers: [BlockService, HeartService, ReportService],
+})
+export class SocialModule {}

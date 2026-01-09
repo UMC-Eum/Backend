e8ebb1e30d6f2d6994d45834bbe4f1e8ae80f229
eschoi04 <eunsongchoi04@gmail.com>
2026-01-09 19:27:55 +0900

    feat: finished implementing api/v1/notifications

--- src/modules/app/app.module.ts
+++ src/modules/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { envSchema } from '../../config/env.schema';
 import { HealthModule } from '../health/health.module';
 import { PrismaModule } from 'src/infra/prisma/prisma.module';
+import { NotificationModule } from '../notification/notification.module';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { PrismaModule } from 'src/infra/prisma/prisma.module';
     }),
     HealthModule,
     PrismaModule,
+    NotificationModule,
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/modules/notification/controllers/notification.controller.ts
+++ src/modules/notification/controllers/notification.controller.ts
@@ -18,12 +18,27 @@ export class NotificationController {
   markAsRead(@Param('id') id: string) {
     return this.notificationService.markAsRead(id);
   }
-
-  @Get()
+// AUTH가 구현되면 사용할 코드
+  /*@Get()
+  
   findAll( @Req() req, @Query('cursor') cursor?: string, @Query('size') size?: string) {
     const limit = size ? Number(size) : 20;
     return this.notificationService.findAll(req.user.id, cursor, limit);
-  }
+  }*/
+ // AUTH구현되기 전 임시 코드
+ @Get()
+  findAll(
+  @Body('userId') userId: number,
+  @Query('cursor') cursor?: string,
+  @Query('size') size?: string,
+  ) {
+  return this.notificationService.findAll(
+    userId,
+    cursor,
+    size ? Number(size) : 20,
+  );
+}
+
   @Patch()
   update(@Body() dto: UpdateNotificationDto) {
     return this.notificationService.update(dto);

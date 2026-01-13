da56069c80bf8dc30239d87402efb771f8e09e23
annalee8595 <yenalee8595@gmail.com>
2026-01-13 17:49:07 +0900

    feat: add AccessTokenGuard for token validation and user authentication

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { AuthLogoutController } from './controllers/auth-logout.controller';
 import { AuthTokenController } from './controllers/auth-token.controller';
 import { KakaoAuthController } from './controllers/kakao-auth.controller';
+import { AccessTokenGuard } from './guards/access-token.guard';
 import { AuthTokenService } from './services/auth-token.service';
 import { JwtTokenService } from './services/jwt-token.service';
 import { KakaoAuthService } from './services/kakao-auth.service';
@@ -10,6 +11,12 @@ import { PrismaModule } from '../../infra/prisma/prisma.module';
 @Module({
   imports: [PrismaModule],
   controllers: [KakaoAuthController, AuthTokenController, AuthLogoutController],
-  providers: [KakaoAuthService, AuthTokenService, JwtTokenService],
+  providers: [
+    KakaoAuthService,
+    AuthTokenService,
+    JwtTokenService,
+    AccessTokenGuard,
+  ],
+  exports: [JwtTokenService, AccessTokenGuard],
 })
 export class AuthModule {}

9c58fed10d9efed375c598b5a945fa54151a16fd
eschoi04 <eunsongchoi04@gmail.com>
2026-01-25 19:08:15 +0900

    feat: remove unnecessary columns

---
+++ prisma/migrations/20260125100717_remove_unnecessary_columns/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `vibeVector` on the `userinterest` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `userinterest` DROP COLUMN `vibeVector`;

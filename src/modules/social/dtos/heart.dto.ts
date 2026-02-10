aa54dee52e9c5209760dca22f93d8562847dfdd4
ehgus3193 <ehgus3193@naver.com>
2026-02-11 05:14:31 +0900

    refactor : get Heart 기능 추가

--- src/modules/social/dtos/heart.dto.ts
+++ src/modules/social/dtos/heart.dto.ts
@@ -1,3 +1,9 @@
+export interface UserProfileInfo {
+  profileImageUrl: string | null;
+  nickname: string;
+  age: number;
+}
+
 export interface HeartItemBase {
   heartId: number;
   createdAt: string;
@@ -5,10 +11,12 @@ export interface HeartItemBase {
 
 export interface HeartSentItem extends HeartItemBase {
   targetUserId: number;
+  targetUser: UserProfileInfo;
 }
 
 export interface HeartReceivedItem extends HeartItemBase {
   fromUserId: number;
+  fromUser: UserProfileInfo;
 }
 
 export interface HeartListPayload<T extends HeartItemBase> {

e956fb685e1a9590ca3b44b379f6a022850826f9
eschoi04 <eunsongchoi04@gmail.com>
2026-05-09 22:55:12 +0900

    feat: initial migration

--- prisma/migrations/20260217094547_add_sent_by_in_notification/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `Notification` ADD COLUMN `sentById` BIGINT NULL;
-
--- CreateIndex
-CREATE INDEX `Notification_sentById_idx` ON `Notification`(`sentById`);
-
--- AddForeignKey
-ALTER TABLE `Notification` ADD CONSTRAINT `Notification_sentById_fkey` FOREIGN KEY (`sentById`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
